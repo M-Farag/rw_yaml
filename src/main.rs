23f18c52875f57cf7666e40102ab69a25a5c0db0
M-Farag <mina.farag@icloud.com>
2023-06-28 12:42:55 +0300

    UPD Reading a yml file and printing all the tasks

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use std::fs;
+
 use serde::{Deserialize, Serialize};
 use serde_yaml;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct task {
     name: String,
+
+    #[serde(skip)]
     done: bool,
     tags: Vec<String>,
 }
@@ -14,5 +18,11 @@ struct TaskList {
 }
 
 fn main() {
-    
+    // read yaml file
+    let file = fs::File::open("tasks.yml").expect("Err: Reading the file");
+    let tasks:TaskList = serde_yaml::from_reader(file).expect("Err: Serializing the yml file");
+
+    for task in tasks.tasks {
+        println!("{:#?}",task);
+    }
 }
