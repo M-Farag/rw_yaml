68da340842a48002cdbc5a71e7f801368a02c87e
M-Farag <mina.farag@icloud.com>
2023-06-28 12:35:59 +0300

    ADD Task file tasks.yml to contain the tasks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use serde::{Deserialize, Serialize};
+use serde_yaml;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct task {
+    name: String,
+    done: bool,
+    tags: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct TaskList {
+    tasks: Vec<task>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    
 }
